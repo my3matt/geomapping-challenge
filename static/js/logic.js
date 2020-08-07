6d8fd665ea139f016c56725758f805bd7b3e0552
Maithri Mathew <maithreyi.mathew@gmail.com>
2020-08-07 17:57:20 -0400

    Couldn't fix config.js

--- static/js/logic.js
+++ static/js/logic.js
@@ -12,7 +12,7 @@ var myMap = L.map("map", {
     attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
     maxZoom: 18,
     id: "mapbox.streets",
-    accessToken: API_KEY
+    accessToken: "pk.eyJ1IjoiY2hpYW15YzA5ODciLCJhIjoiY2swdzUxb3I2MGRiMzNpbnliN293OXBteiJ9.at8rk5Trv5oNH1dD2E9EAw"
   }).addTo(myMap);
 
   // all week earthquake data 
